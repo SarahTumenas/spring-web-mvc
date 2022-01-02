3c959348a302b5b92e01b89323cda217e20241b0
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-02 09:52:07 -0300

    Controller

---
+++ src/main/java/com/github/SarahTumenas/springwebmvc/controller/JediController.java
@@ -0,0 +1,13 @@
+package com.github.SarahTumenas.springwebmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JediController {
+
+    @GetMapping ("/jedi")
+    public String jedi(){
+        return "jedi";
+    }
+}
