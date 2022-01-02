573ee03116bc9f6734fb81068d1f7b0776054c5e
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-02 11:45:41 -0300

    refatorando

--- src/main/java/com/github/SarahTumenas/springwebmvc/controller/JediController.java
+++ src/main/java/com/github/SarahTumenas/springwebmvc/controller/JediController.java
@@ -4,13 +4,15 @@ import com.github.SarahTumenas.springwebmvc.model.Jedi;
 import com.github.SarahTumenas.springwebmvc.repository.JediRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class JediController {
@@ -29,17 +31,15 @@ public class JediController {
     }
 
     @GetMapping ("/new-jedi")
-    public ModelAndView newJedi (){
-        final ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("new-jedi");
+    public String createJedi(Model model) {
 
-        modelAndView.addObject("jedi", new Jedi());
+        model.addAttribute("jedi", new Jedi());
 
-        return modelAndView;
+        return "new-jedi";
     }
 
     @PostMapping("/jedi")
-    public String createJedi(@Validated @ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirect){
+    public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirect){
 
         if(result.hasErrors()){
             return "new-jedi";
