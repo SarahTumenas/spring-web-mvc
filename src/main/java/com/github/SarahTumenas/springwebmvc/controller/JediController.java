8f2aee11b54f99795670481faec3658e12260180
SarahTumenas <sarahtumenas@yahoo.com.br>
2022-01-02 10:19:16 -0300

    Mapping

--- src/main/java/com/github/SarahTumenas/springwebmvc/controller/JediController.java
+++ src/main/java/com/github/SarahTumenas/springwebmvc/controller/JediController.java
@@ -1,13 +1,32 @@
 package com.github.SarahTumenas.springwebmvc.controller;
 
+import com.github.SarahTumenas.springwebmvc.model.Jedi;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class JediController {
 
     @GetMapping ("/jedi")
-    public String jedi(){
-        return "jedi";
+    public ModelAndView jedi(){
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("jedi");
+
+        final Jedi luke =new Jedi("Luke", "Skywalker");
+        modelAndView.addObject("allJedi", List.of(luke));
+
+        return modelAndView;
+    }
+    @GetMapping ("/new-jedi")
+    public ModelAndView newJedi (){
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("new-jedi");
+
+        modelAndView.addObject("jedi", new Jedi());
+
+        return modelAndView;
     }
 }
